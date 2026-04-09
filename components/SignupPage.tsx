34c48ec30e48e5b0b0d2b0ad41d284b2dfde238f
jhomaa <adelzarajhonmarcel@gmail.com>
2026-04-09 20:38:22 +0800

    add a simple ui for the sign up page

--- components/SignupPage.tsx
+++ components/SignupPage.tsx
@@ -1,8 +1,25 @@
-import React from "react"
-import {  Text } from "react-native";
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
 
 export function Signup() {
-    return(
-        <Text>hello world</Text>
-    )
+  return (
+    <View>
+      <Text style={styles.Text}>🫵</Text>
+      <Text style={styles.Textcome}>bukas na to putang ina</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  Text: {
+    fontSize: 100,
+    marginTop: 250,
+    marginLeft: 140,
+  },
+
+  Textcome: {
+    fontSize: 30,
+    marginTop: 20,
+    marginLeft: 50,
+  }
+});
